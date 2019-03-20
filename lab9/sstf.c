5ca61d2f52cdde26122acaf07597730d292a5cb1
Rahul <rsrssingh419@gmail.com>
2019-03-20 15:57:37 +0530

    lab 9 disk scheduling programs

---
+++ lab9/sstf.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+struct Node
+{
+	int val;
+	int distance;
+	int accessed;
+}A[100];
+void sstf(int head,int N)
+{
+	int i,j,k=0,seek=0,min=10000;
+	for(i=0;i<N;i++)
+	{
+		// picking the nearest request that has not yet been accessed
+		for(j=0;j<N;j++)
+		{
+			if(A[j].accessed == 0 && A[j].distance < min)
+			{
+				min = A[j].distance;
+				k = j;
+			}
+		}
+		// moving the head
+		if(A[k].val < head)
+		printf("Moving left\n");
+		else
+		printf("Moving right\n");
+		head = A[k].val;
+		printf("%d ",head);
+		A[k].accessed = 1;
+		seek += A[k].distance;
+		min = 10000;
+		for(j=0;j<N;j++)
+		{
+			if(A[j].accessed == 0)
+			A[j].distance = abs(A[j].val - head);
+		}						
+	}
+	printf("Number of seek operations = %d.\n",seek);
+}
+void main()
+{
+	int N,i,j,k,head;
+	printf("Enter the number of request sequence: ");
+	scanf("%d",&N);
+	printf("Enter %d requests: ",N);
+	for(i=0;i<N;i++)
+	scanf("%d",&A[i].val);
+	printf("Enter the head position: ");
+	scanf("%d",&head);
+	for(i=0;i<N;i++)
+	{
+		A[i].accessed = 0;
+		A[i].distance = abs(A[i].val - head);
+	}
+	printf("%d ",head);
+	sstf(head,N);
+}
